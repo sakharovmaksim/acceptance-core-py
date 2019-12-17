bf9fd1f28ab4c6a38eb4956ba02dc659153bc2d3
m.sakharov <m.sakharov@butik.ru>
2019-12-17 16:21:05 +0300

    Started commit

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# acceptance-core-py
+# Acceptance Core For Selenium. Powered by Python
+=================================================
+## Requirements
+Python 3.8+
+
+## Install
+### Install virtualenv
+`pip install virtualenv`
+
+### Create virtualenv dir
+`virtualenv venv`
+
+### Run virtualenv
+`source venv/bin/activate`
+
+### Install requirements
+`pip install -r requirements.txt`
+
+### Run your tests with Docker in easy way
+`sh run_tests_in_docker.sh stage_pytest.ini`
+
+### Run your tests with local Python
+`pytest -c stage_pytest.ini -s` – with config `stage_pytest.ini` and show `print` logs 
---
+++ run_tests_in_docker.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+if [ -z "$1" ]; then
+    echo "Please, set config file! Example: sh run_tests_in_docker.sh prod_pytest.ini"
+    exit 1
+fi
+
+pwd=`pwd`
+
+TAG="sakharovmaksim/service-integration-tests-base-image-python:latest"
+echo "Локальный запуск api-тестов, используя docker-image $TAG"
+
+CONTAINER_NAME="my_local_acceptance_tests_run"
+
+# Устанавливаем обработчик сигнала, чтобы контейнер удалился, даже если скрипт завершают досрочно
+trap 'docker rm -f $CONTAINER_NAME' 2 15
+
+docker pull $TAG
+
+docker run -di --net=host --name=$CONTAINER_NAME -v $pwd:/local_project $TAG
+
+# Копируем код тестов из --volume папки в папку с предустановленным vendor, чтобы не требовать vendor на хосте
+docker exec $CONTAINER_NAME rsync -a /local_project/. /tests-core-dir/ --exclude venv --exclude output --exclude tmp --exclude .git
+
+# Запуск тестов из папки, в которой предустановлен Python с дополнениями. Установлена опция --html формирования HTML-отчета (можно выключить)
+docker exec $CONTAINER_NAME pytest /tests-core-dir/ -s --html=/tests-core-dir/output/report.html --self-contained-html \
+    -c /tests-core-dir/$1
+
+# Копируем репорты от тестов обратно в --volume-папку local_project
+docker exec $CONTAINER_NAME rsync -a /tests-core-dir/output/ /local_project/output/
+
+echo "Останавливается и удаляется docker-контейнер $CONTAINER_NAME"
+docker rm -f $CONTAINER_NAME
